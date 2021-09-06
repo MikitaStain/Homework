517fc6e429c7944d85d48f71796ff9450ebc2430
Mikita Stain <MikitaStain@list.ru>
2021-09-06 09:45:59 +0300

    homework 6 start

---
+++ homework_6_EmployeesDB/src/main/java/by/itacademy/employees/web/servlet/EmployeesServlet.java
@@ -0,0 +1,25 @@
+package by.itacademy.employees.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EmployeesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String salary = req.getParameter("salary");
+
+        
+
+
+    }
+}
